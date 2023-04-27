8a0751df2142d8ba4b56d550e19970ee6e86f22f
Riguz Lee <lihaifeng2@wps.cn>
2023-04-27 16:39:07 +0800

    Added empty ts project

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="contained">Hello World</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
